dca0225316c3ae17d751fcd34115758470784969
Luke Taylor <99ltaylor@gmail.com>
2021-08-19 15:02:08 +0200

    amended missing spaces

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,15 +2,15 @@ require_relative "../lib/fizzbuzz"
 
 describe "fizzbuzz" do
   it 'returns "fizz" when passed 3' do
-    expect (fizzbuzz(3)).to eq("fizz")
+    expect(fizzbuzz(3)).to eq("fizz")
   end
   it 'returns "buzz" when passed 5' do
-    expect (fizzbuzz(5)).to eq("buzz")
+    expect(fizzbuzz(5)).to eq("buzz")
   end
   it 'returns "fizzbuzz" when passed 15' do
-    expect (fizzbuzz(15)).to eq("fizzbuzz")
+    expect(fizzbuzz(15)).to eq("fizzbuzz")
   end
   it 'return "22" when passed 22' do
-    expect (fizzbuzz(22)).to eq(22)
+    expect(fizzbuzz(22)).to eq(22)
   end
 end
